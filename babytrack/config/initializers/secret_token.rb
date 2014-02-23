437716330ef88d22b974d85e885a13ca6670a385
JavierJia <jianfeng.jia@gmail.com>
2014-02-22 20:27:00 -0800

    initial

---
+++ babytrack/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babytrack::Application.config.secret_key_base = '96bde363f1c88d6e4399d00220883e828ac91f0af73d6c8a7895d47693f4936f702f0d4ee48c9af90a1941be2a414df5811c35cdf787d343c470e2e28087e83a'
